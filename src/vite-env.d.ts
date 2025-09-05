99ff17911a3f4d46e860574179f9f4b3a30dd4e4
Nathan Rosales <nathannrosales17@gmail.com>
2025-09-05 13:44:34 -0500

    maybe fixed?

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USER_POOL_ID: string
+  readonly VITE_USER_POOL_CLIENT_ID: string
+  readonly VITE_IDENTITY_POOL_ID: string
+  readonly VITE_AWS_REGION: string
+  readonly VITE_APPSYNC_URL: string
+  readonly VITE_APPSYNC_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+
